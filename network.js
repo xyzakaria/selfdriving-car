011ffd55cc024e27a78c0433459e819f6b9f9b88
xyzakaria <83395838+xyzakaria@users.noreply.github.com>
2024-10-12 19:06:00 +0200

    bdr
    drb

--- network.js
+++ network.js
@@ -39,17 +39,14 @@ class Level {
             for(let j = 0 ; j<level.inputs.length;j++){
                 sum+=level.inputs[j]*level.weights[j][i];
 
-            }
-        }   
+            }  
             if(sum>level.biases[i]){
                 level.outputs[i]=1;
             }
             else{
                 level.outputs[i]=0;
             }
-        return level.outputs;
-        
+        }
+        return level.outputs;      
     }
-
-
 }
